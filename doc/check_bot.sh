5439e5f62314f5384f6e95c97cd7f87b23ac4449
anyongjin <anyongjin163@163.com>
2024-04-04 13:36:25 +0800

    update usage.md

---
+++ doc/check_bot.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 name to_user1 [to_user2] ..."
+    exit 1
+fi
+
+# extract name and to_user from arguments
+name="$1"
+shift
+to_users=("$@")
+
+status_file="/tmp/bot_${name}.txt"
+
+if [ -f "$status_file" ]; then
+    last_status=$(cat $status_file)
+else
+    last_status="running"
+fi
+
+if pgrep -f "${name}.yml" &>/dev/null; then
+    status="running"
+    body="next notify would be sent when the bot is stopped"
+else
+    status="stopped"
+    body="contact administrator please"
+fi
+
+echo "$status" > "$status_file"
+if [ "$last_status" != "$status" ]; then
+    for user in "${to_users[@]}"; do
+        echo "$body"| mail -s "bot $status: $name" "$user"
+    done
+fi
--- doc/usage.md
+++ doc/usage.md
@@ -12,3 +12,24 @@
 ```shell
 banbot cmp_orders --exg-path=[实盘委托记录.xlsx] --bt-path=[orders_-1.csv]
 ```
+### 如何在机器人停止时发邮件通知？
+```shell
+yum install sendmail sendmail-cf mailx
+
+# 设置发件人信息
+vim /etc/mail.rc
+
+set from=sender_addr
+set smtp=smtp.server.com
+set smtp-auth-user=sender_addr
+set smtp-auth-password=sender_password
+set smtp-auth=login
+
+# 查看邮件
+tail /var/log/maillog
+```
+然后将`check_bot.sh`复制到服务器，并执行`chmod +x check_bot.sh`授予可执行权限。  
+然后在corntab中每隔5分钟执行脚本检查状态：
+```text
+3-58/5 * * * * /path_to/check_bot.sh bot1 user1@xxx.com user2@xxx.com
+```
